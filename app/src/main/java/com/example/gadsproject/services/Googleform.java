1c25b1d21944e5f182c47db5d56cef73914e8495
MUHAMMAD DINI IBRAHIM <muhammaddibrahim2@gmail.com>
2020-09-10 14:56:03 +0100

    Update Googleform.java
    Form update

--- app/src/main/java/com/example/gadsproject/services/Googleform.java
+++ app/src/main/java/com/example/gadsproject/services/Googleform.java
@@ -11,10 +11,10 @@ public interface Googleform {
 
     @POST("https://docs.google.com/forms/d/e/1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse")
     @FormUrlEncoded
-    Call<myinfo> gform(
+    Call<Void> gform(
             @Field("entry.1824927963") String fmail,
             @Field("entry.1877115667") String fname,
-            @Field("entry.261820127") String flname,
+            @Field("entry.2006916086") String flname,
             @Field("entry.284483984") String flink
 
     );
